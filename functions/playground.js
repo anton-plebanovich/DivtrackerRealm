086cf5614ad4079395b1ba9ac96f29e06def1980
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-10-24 00:47:42 +0300

    - Redeploy

--- functions/playground.js
+++ functions/playground.js
@@ -1,7 +1,8 @@
 
-exports = async function(){
+// playground.js
+
+exports = async function() {
   context.functions.execute("utils");
   
-  console.log(context.user.stringify())
-  // validateTransactions([{ _id: BSON.ObjectId("607afe635ed931675acfff1c"), _p: "614b283c15a0dc11514db030", s: "AAPL", e: "NAS", a: 1.0, d: new Date(), p: 1.0 }]);
+  console.log(context.user.stringify());
 };
