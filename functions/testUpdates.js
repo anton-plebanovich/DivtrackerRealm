ee5841968a5ab1eb4dc82440c9adba3965f8fad0
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-12-10 19:58:08 +0300

    - Tests fix

--- functions/testUpdates.js
+++ functions/testUpdates.js
@@ -9,7 +9,7 @@
   context.functions.execute("utilsV2");
 
   // TODO: Make it more complex and move to separate test case
-  await Promise.all([
+  return await Promise.all([
     context.functions.execute("updateCompaniesV2"),
     context.functions.execute("updateDividendsV2"),
     context.functions.execute("updatePricesV2"),
@@ -17,10 +17,4 @@
     context.functions.execute("updateSplitsV2"),
     context.functions.execute("updateSymbolsV2"),
   ]);
-  
-  if (errors.length) {
-    throw errors;
-  } else {
-    console.log("SUCCESS!");
-  }
 };
