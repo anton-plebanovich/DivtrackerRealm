bb29ec76bd47e655f2b08a253394088fe8917a5b
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-12-10 19:42:38 +0300

    - Update remove

--- functions/testAddTransactions.js
+++ functions/testAddTransactions.js
@@ -66,17 +66,6 @@
   if (quotesCount !== companiesCount) {
     errors.push([`Some quotes weren't inserted (${quotesCount}/${companiesCount})`]);
   }
-
-  // Check update functions
-  // TODO: Make it more complex and move to separate test case
-  await Promise.all([
-    context.functions.execute("updateCompaniesV2"),
-    context.functions.execute("updateDividendsV2"),
-    context.functions.execute("updatePricesV2"),
-    context.functions.execute("updateQuotesV2"),
-    context.functions.execute("updateSplitsV2"),
-    context.functions.execute("updateSymbolsV2"),
-  ]);
   
   if (errors.length) {
     throw errors;
