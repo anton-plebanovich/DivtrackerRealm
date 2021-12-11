4dd43f52406490e1f1e3f47fb197079ff2ab2b0d
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-12-11 19:16:12 +0300

    - V1 phase 2 migrations

--- functions/migrations.js
+++ functions/migrations.js
@@ -6,14 +6,57 @@
 // https://docs.mongodb.com/manual/reference/method/Bulk.find.removeOne/
 // https://docs.mongodb.com/manual/reference/method/Bulk.insert/
 
-// Operations to perform when V1 is deprecated and sync will be stop:
-// - Remove _p key from all entities
-// - Remove/replace _p logic in all functions
-// - Sync should start to use _ key for partition
-// - All V1 functions, triggers and schemes should be removed
-// - 'divtracker' database should be removed
-exports = async function() {
-  context.functions.execute("utils");
+// V1 deprecation phase 1
+// - V1 functions and triggers should be removed
+// - V1 data should be erased in all collections
+// - Check that V2 data is properly fetched, e.g. enough dividends and historical prices
 
-  
+// V1 deprecation phase 2
+// - Manual: Disable sync
+// - Manual: Drop `divtracker` database
+// - Manual: Run data migrations
+// - Deploy: New schemes, function, and sync that are using optional _ partition key. V1 schemes remove.
+exports = async function() {
+  context.functions.execute("utilsV2");
+
+  const operations = [];
+
+  // Deleting _p key in all data collections
+  const dataCollections = [
+    db.collection('companies'),
+    db.collection('dividends'),
+    db.collection('exchange-rates'),
+    db.collection('historical-prices'),
+    db.collection('previous-day-prices'),
+    db.collection('quotes'),
+    db.collection('splits'),
+    db.collection('symbols'),
+    db.collection('splits'),
+  ];
+
+  for (const collection of dataCollections) {
+    const query = {};
+    const update = { $unset: { "_p": "" } };
+    const options = { "upsert": false }
+    operations.push(
+      itemsCollection.updateMany(query, update, options)
+    );
+  }
+
+  // Renaming _p key to _ in all user collections
+  const userCollections = [
+    db.collection('settings'),
+    db.collection('transactions'),
+  ];
+
+  for (const collection of dataCollections) {
+    const query = {};
+    const update = { $unset: { _p: "" }, $set: { _: "$_p" } };
+    const options = { "upsert": false }
+    operations.push(
+      itemsCollection.updateMany(query, update, options)
+    );
+  }
+
+  return Promise.all(operations);
 };
