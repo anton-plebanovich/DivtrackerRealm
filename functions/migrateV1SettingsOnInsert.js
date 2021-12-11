f0690f0e2dcb806dfadc85150d6dde5ed23949eb
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-12-11 18:48:43 +0300

    - Migrate V1 -> V2 settings on insert or edit

---
+++ functions/migrateV1SettingsOnInsert.js
@@ -0,0 +1,26 @@
+
+// migrateV1SettingsOnInsert.js
+
+// https://docs.mongodb.com/manual/reference/method/Bulk.find.upsert/
+// https://docs.mongodb.com/realm/mongodb/actions/collection.bulkWrite/
+// https://docs.mongodb.com/realm/mongodb/actions/collection.insertMany/
+
+exports = async function(changeEvent) {
+  context.functions.execute("utilsV2");
+
+  const settings = changeEvent.fullDocument;
+  const customTaxes = settings.ts;
+  if (customTaxes == null || !customTaxes.length) { return; }
+
+  const settingsV2 = Object.assign({}, settings);
+  settingsV2.ts = [];
+
+  const symbolsCollection = db.collection("symbols");
+  for (const customTax in customTaxes) {
+    const symbol = await symbolsCollection.findOne({ t: customTax.s });
+    const symbolID = symbol._id;
+    settingsV2.ts.push({ s: symbolID, t: customTax.t });
+  }
+  
+  return await db.collection("settings").safeUpdateMany([settings]);
+};
