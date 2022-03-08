949c36205f983bc5aebee8aa1b722c7b6e5831de
Anton Plebanovich <anton.plebanovich@gmail.com>
2022-03-08 19:17:20 +0300

    [FMP] Quotes update

---
+++ functions/fmpUpdateQuotes.js
@@ -0,0 +1,28 @@
+
+// fmpUpdateQuotes.js
+
+// https://docs.mongodb.com/manual/reference/method/js-collection/
+// https://docs.mongodb.com/manual/reference/method/js-bulk/
+// https://docs.mongodb.com/manual/reference/method/db.collection.initializeUnorderedBulkOp/
+// https://docs.mongodb.com/realm/mongodb/
+// https://docs.mongodb.com/realm/mongodb/actions/collection.bulkWrite/#std-label-mongodb-service-collection-bulk-write
+// https://docs.mongodb.com/realm/mongodb/actions/collection.find/
+
+exports = async function() {
+  context.functions.execute("fmpUtils");
+
+  const shortSymbols = await getShortSymbols();
+  if (shortSymbols.length <= 0) {
+    console.log(`No symbols. Skipping update.`);
+    return;
+  }
+
+  // TODO: We might insert after each batch fetch
+  const quotes = await fetchQuotes(shortSymbols);
+  const quotesCollection = db.collection("quotes");
+  await quotesCollection.safeUpsertMany(quotes);
+
+  await setUpdateDate("fmp-quotes");
+
+  console.log(`SUCCESS`);
+};
