c1ed3bcb17cbb96caeb7cf14c784e050f94625fe
Anton Plebanovich <anton.plebanovich@gmail.com>
2022-03-19 11:23:47 +0300

    - After merge fix

--- functions/testUpdates.js
+++ functions/testUpdates.js
@@ -13,7 +13,8 @@
   // TODO: Make it more complex and move to separate test case
   await Promise.all([
     context.functions.execute("updateCompaniesV2"),
-    context.functions.execute("updateDividendsV2"),
+    context.functions.execute("updateDividendsFuture"),
+    context.functions.execute("updateDividendsPast"),
     context.functions.execute("updatePricesV2"),
     context.functions.execute("updateQuotesV2"),
     context.functions.execute("updateSplitsV2"),
