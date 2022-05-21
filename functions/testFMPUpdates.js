2655ae462837c14c069e784adb9cd0bd7a9a0c3f
Anton Plebanovich <anton.plebanovich@gmail.com>
2022-05-21 16:21:28 +0300

    - Lower data pressure for testFMPUpdates

--- functions/testFMPUpdates.js
+++ functions/testFMPUpdates.js
@@ -6,15 +6,24 @@
    exports();
  */
  exports = async function() {
-  context.functions.execute("testUtils");
+  context.functions.execute("fmpUtils");
 
-  // Cleanup environment
-  await cleanup();
+  // Cleanup FMP environment
+  await Promise.all([
+    fmp.collection('companies').deleteMany({}),
+    fmp.collection('data-status').deleteMany({}),
+    fmp.collection('dividends').deleteMany({}),
+    fmp.collection('historical-prices').deleteMany({}),
+    fmp.collection('quotes').deleteMany({}),
+    fmp.collection('splits').deleteMany({}),
+    fmp.collection('symbols').deleteMany({}),
+  ]);
+  
+  // Add some symbols
+  const symbols = await fetchSymbols();
+  const symbolsToAdd = symbols.getRandomElements(20);
+  await fmp.collection('symbols').insertMany(symbolsToAdd);
 
-  const transactions = await generateRandomTransactions();
-  await context.functions.execute("addTransactionsV2", transactions);
-
-  // TODO: Make it more complex and move to separate test case
   await Promise.all([
     context.functions.execute("fmpUpdateCompanies"),
     context.functions.execute("fmpUpdateDividends"),
@@ -23,6 +32,4 @@
     context.functions.execute("fmpUpdateSplits"),
     context.functions.execute("fmpUpdateSymbols"),
   ]);
-
-  await checkData(transactions);
 };
