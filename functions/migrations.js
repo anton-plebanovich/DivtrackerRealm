08426ea04cf8f308d4234b6094ee5c6c4b8090db
Anton Plebanovich <anton.plebanovich@gmail.com>
2022-05-29 13:51:44 +0300

    - Migration steps

--- functions/migrations.js
+++ functions/migrations.js
@@ -12,5 +12,27 @@
 exports = async function() {
   context.functions.execute("utils");
 
-  
+  // Release new server with disabled FMP symbols update
+  // dt backup --environment sandbox-anton --database fmp
+  // dt restore --environment sandbox-anton --database fmp --to-database fmp-tmp
+  // dt call-realm-function --environment sandbox-anton --function fmpUpdateSymbols --argument fmp-tmp --verbose
+  // dt call-realm-function --environment sandbox-anton --function fmpLoadMissingData --argument fmp-tmp --retry-on-error 'execution time limit exceeded'
+  // dt backup --environment sandbox-anton --database fmp-tmp
+  // dt restore --environment local --backup-source-environment sandbox-anton --database fmp-tmp
+  // Execute symbols migration
+  // dt backup --environment local --database fmp-tmp
+  // dt restore --environment sandbox-anton --backup-source-environment local --database fmp-tmp
+  // Check data count
+  // dt call-realm-function --environment sandbox-anton --function fmpLoadMissingData --argument fmp-tmp --verbose
+  // dt call-realm-function --environment sandbox-anton --function fmpUpdateSymbols --argument fmp-tmp --verbose
+  // dt call-realm-function --environment sandbox-anton --function fmpUpdateCompanies --argument fmp-tmp --verbose
+  // dt call-realm-function --environment sandbox-anton --function fmpUpdateDividends --argument fmp-tmp --verbose
+  // dt call-realm-function --environment sandbox-anton --function fmpUpdatePrices --argument fmp-tmp --verbose
+  // dt call-realm-function --environment sandbox-anton --function fmpUpdateQuotes --argument fmp-tmp --verbose
+  // dt call-realm-function --environment sandbox-anton --function fmpUpdateSplits --argument fmp-tmp --verbose
+  // Check data count
+  // dt backup --environment sandbox-anton --database fmp-tmp
+  // dt restore --environment sandbox-anton --database fmp-tmp --to-database fmp
+  // dt call-realm-function --environment sandbox-anton --function checkTransactionsV2 --verbose
+  // Enable FMP symbols update
 };
