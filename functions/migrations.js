0ca2904b9882a5a31f0719185bf8ec13430b4647
Anton Plebanovich <anton.plebanovich@gmail.com>
2022-06-17 14:40:24 +0300

    - Migration fix

--- functions/migrations.js
+++ functions/migrations.js
@@ -459,6 +459,7 @@ function get_duplicated_IEX_dividend_IDs(dividends) {
 }
 
 async function delete_duplicated_IEX_dividends() {
+  const collection = db.collection("dividends");
   const dividends = await collection.fullFind({ x: { $ne: true }, i: { $ne: null } });
   const duplicatedIDs = get_duplicated_IEX_dividend_IDs(dividends);
 
